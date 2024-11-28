6eca39f31a1e765556023f276e5dbee09157e1dc
DevanshMern <srdev98@gmail.com>
2024-11-28 13:29:21 +0530

    Api completed

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
+Run "npm i" to install all the dependencies
 Run "npm run start" command
 
 open Postman localhost port 8080 then hit the endpoint 
 
 post data in body for post request
+
+<!-- http://localhost:8080/addSchool -->
+{
+    "name": "Convent School",
+    "address": "RJPM",
+    "longitude": 12.0,
+    "latitude": 22.0
+}
+
+get request
+http://localhost:8080/listSchools?latitude=0.0&longitude=0.0
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from "express"
+import express, { json } from "express";
 import { connection } from "./connectDB.js";
 var app = express();
-app.use(express.json())
-app.get("/",function(request,response){
-response.send("Hello World!")
-})
+app.use(express.json());
+app.get("/", function (request, response) {
+  response.send("Hello World!");
+});
 
 const addSchools = (request, response) => {
-    console.log(request)
+  console.log(request);
   const { name, address, latitude, longitude } = request.body;
   connection.query(
     "Insert into allschools (name ,address,latitude,longitude) Values(?,?,?,?) ",
@@ -28,7 +28,42 @@ const addSchools = (request, response) => {
     }
   );
 };
+const finalOperation = (response, err, result, x, y) => {
+  if (err) {
+    response.send({
+      status: false,
+      err: err,
+      message: "Error occured",
+    });
+  } else {
+    let schoolsNearby = result
+      .map((items) => {
+        let distance = Math.sqrt(
+          Math.pow(items.latitude - x, 2) + Math.pow(items.longitude - y, 2)
+        ).toFixed(2);
+        return { ...items, distance };
+      })
+      .sort((a, b) => {
+        return parseFloat(a.distance) - parseFloat(b.distance);
+      });
+    response.send({
+      status: true,
+      message: "fetched Success",
+      data: schoolsNearby,
+    });
+  }
+};
+const getSchools = (request, response) => {
+  const { latitude: x, longitude: y } = request.query;
+  connection.query(
+    "select name,address,latitude,longitude from allSchools",
+    (err, result) => {
+      finalOperation(response, err, result, x, y);
+    }
+  );
+};
 app.post("/addSchool", addSchools);
+app.get("/listSchools", getSchools);
 app.listen(8080, function () {
   console.log("Started application on port %d", 8080);
 });
